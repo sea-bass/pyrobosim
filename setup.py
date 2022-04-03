8803504f249a5add8c1fefa0882e39e3eefe2dc5
Sebastian Castro <sebas.a.castro@gmail.com>
2022-04-03 10:45:43 -0400

    Switch to BSD license

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     author="Sebastian Castro",
     author_email="sebas.a.castro@gmail.com",
     description="ROS2 enabled 2D mobile robot simulator for behavior prototyping.",
-    license="MIT",
+    license="BSD",
     packages=[
         project_name,
         project_name + "/gui",
