cf110ccc9751c7287532b8cd440327d71e80b083
Sebastian Castro <sebas.a.castro@gmail.com>
2022-05-30 12:46:17 -0400

    Ensure Docker display works without xhost command

--- docker/build_docker.bash
+++ docker/build_docker.bash
@@ -9,5 +9,6 @@ IMAGE_NAME=pyrobosim_$ROS_DISTRO
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 pushd $SCRIPT_DIR/..
-docker build -f $SCRIPT_DIR/Dockerfile.$ROS_DISTRO -t $IMAGE_NAME .
+docker build -f $SCRIPT_DIR/Dockerfile.$ROS_DISTRO \
+             -t $IMAGE_NAME .
 popd
