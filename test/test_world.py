f20a02419ac27fd9c522cca9af7e095a52252f85
V Mohammed Ibrahim <12377945+ibrahiminfinite@users.noreply.github.com>
2022-08-04 10:25:31 +0530

    Update with review changes.

--- test/test_world.py
+++ test/test_world.py
@@ -12,22 +12,17 @@ from pyrobosim.utils.general import get_data_folder
 def test_create_world():
     data_folder = get_data_folder()
     world = World()
-    # Set the location and object metadata
-    world.set_metadata(locations=os.path.join(data_folder, "example_location_data.yaml"),
-                       objects=os.path.join(data_folder, "example_object_data.yaml"))
 
     assert World is not None
 
-
 def test_create_room():
     world = World()
-    room_cords = [(-1, -1), (1.5, -1), (1.5, 1.5), (0.5, 1.5)]
+    room_name = "room1"
+    room_coords = [(-1, -1), (1.5, -1), (1.5, 1.5), (0.5, 1.5)]
     room = Room(room_cords,
-                name="room_1",
+                name=room_name,
                 color=[1, 0, 0])
     world.add_room(room)
 
     assert len(world.rooms) == 1
-    assert world.get_room_by_name("room_1") == room
-
-    
+    assert world.get_room_by_name(room_name) == room
