a358ba6a549d98fe08505ba355233a3608d6f4cd
AndyZe <zelenak@picknik.ai>
2022-09-23 15:13:31 -0500

    Only allow world loading via datafile, not programmatically

--- pyrobosim_ros/examples/demo.py
+++ pyrobosim_ros/examples/demo.py
@@ -20,63 +20,10 @@ from pyrobosim.utils.pose import Pose
 data_folder = get_data_folder()
 
 
-def create_world():
-    """ Create a test world """
-    w = World()
-
-    # TODO(andyz): load test_world.yaml, since that's what exports to Gazebo
-
-    # Set the location and object metadata
-    w.set_metadata(locations=os.path.join(data_folder, "example_location_data.yaml"),
-                   objects=os.path.join(data_folder, "example_object_data.yaml"))
-
-    # Add rooms
-    r1coords = [(-2.0, -6.0), (5.0, -6.0), (5.0, 1.5), (-2.0, 1.5)]
-    w.add_room(Room(r1coords, name="kitchen", color=[1, 0, 0],
-               nav_poses=[Pose(x=0.75, y=0.75, yaw=0)]))
-    r2coords = [(1.75, 2.5), (7.0, 2.5), (7.0, 6.0), (1.75, 6.0)]
-    w.add_room(Room(r2coords, name="bedroom", color=[0, 0.6, 0]))
-    r3coords = [(0.0, 2.0), (0.0, 7.0), (-5.0, 7.0), (-5.0, 2.0)]
-    w.add_room(Room(r3coords, name="bathroom", color=[0, 0, 0.6]))
-
-    # Add hallways between the rooms
-    w.add_hallway("kitchen", "bathroom", width=2.0,
-                  conn_method="auto")
-    w.add_hallway("bathroom", "bedroom", width=2.0,
-                  conn_method="auto")
-    w.add_hallway("kitchen", "bedroom", width=2.0,
-                  conn_method="auto")
-
-    # Add locations
-    table = w.add_location("table", "kitchen", Pose(
-        x=0.85, y=-0.5, yaw=-np.pi/2))
-    desk = w.add_location("desk", "bedroom", Pose(x=3.15, y=3.65, yaw=0))
-    counter = w.add_location("counter", "bathroom", Pose(
-        x=-2.45, y=2.5, yaw=np.pi/2 + np.pi/16))
-
-    # Add objects
-    w.add_object("banana", table, pose=Pose(x=1.0, y=-0.5, yaw=np.pi/4))
-    w.add_object("apple", desk, pose=Pose(x=3.2, y=3.5, yaw=0))
-    w.add_object("apple", table)
-    w.add_object("apple", table)
-    w.add_object("water", counter)
-    w.add_object("banana", counter)
-    w.add_object("water", desk)
-
-    # Add a robot
-    r = Robot(radius=0.1, path_executor=ConstantVelocityExecutor())
-    w.add_robot(r, loc="kitchen")
-
-    # Create a search graph
-    w.create_search_graph(
-        max_edge_dist=3.0, collision_check_dist=0.05, create_planner=True)
-    return w
-
-
 def create_world_from_yaml(world_file):
     from pyrobosim.core.yaml import WorldYamlLoader
     loader = WorldYamlLoader()
-    return loader.from_yaml(os.path.join(data_folder, world_file))
+    return loader.from_yaml(os.path.join(data_folder, world_file + "_world.yaml"))
 
 
 def start_gui(world, args):
@@ -98,8 +45,8 @@ def create_ros_node():
     # Set the world
     world_file = node.get_parameter("world_file").value
     if world_file == "":
-        node.get_logger().info("Creating demo world programmatically.")
-        w = create_world()
+        node.get_logger().error("A world_file parameter was not given.")
+        sys.exit()
     else:
         node.get_logger().info(f"Using world file {world_file}.")
         w = create_world_from_yaml(world_file)
