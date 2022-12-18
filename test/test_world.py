1a5effbe2716827a52e8b2891674727a16a2028c
V Mohammed Ibrahim <12377945+ibrahiminfinite@users.noreply.github.com>
2022-12-18 06:06:21 +0000

    Add test dependencies

--- test/test_world.py
+++ test/test_world.py
@@ -2,20 +2,21 @@
 Unit tests for core world modeling
 """
 
-import os
 import pytest
 from pyrobosim.core.world import World
 from pyrobosim.core.world import Room
-from pyrobosim.utils.general import get_data_folder
 
-
-def test_create_world():
-    data_folder = get_data_folder()
+@pytest.mark.dependency(name="test_create_world_default")
+def test_create_world_default():
+    """ Tests the creation of a world """
+    
     world = World()
+    assert world is not None
 
-    assert World is not None
-
+@pytest.mark.dependency(depends=["test_create_world_default"])
 def test_create_room():
+    """ Tests the creation of a room """
+
     world = World()
     room_name = "room1"
     room_coords = [(-1, -1), (1.5, -1), (1.5, 1.5), (0.5, 1.5)]
@@ -26,3 +27,4 @@ def test_create_room():
 
     assert len(world.rooms) == 1
     assert world.get_room_by_name(room_name) == room
+    
