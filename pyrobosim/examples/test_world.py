e3ca80127d4a24c25c86bf6740f88fbb828e2a28
V Mohammed Ibrahim <12377945+ibrahiminfinite@users.noreply.github.com>
2022-12-30 04:52:36 +0000

    Fix spellings and remove comment

--- pyrobosim/examples/test_world.py
+++ pyrobosim/examples/test_world.py
@@ -7,7 +7,7 @@ from pyrobosim.core.world import World
 from pyrobosim.core.world import Room
 
 
-class TestWorldModelling():
+class TestWorldModeling():
     """ Tests for the world modelling tools"""
 
     @staticmethod
@@ -15,41 +15,40 @@ class TestWorldModelling():
     def test_create_world_default():
         """ Tests the creation of a world """
         
-        TestWorldModelling.world = World()
-        assert TestWorldModelling.world is not None
+        TestWorldModeling.world = World()
+        assert TestWorldModeling.world is not None
 
     
     @staticmethod
-    @pytest.mark.dependency(depends=["TestWorldModelling::test_create_world_default"])
+    @pytest.mark.dependency(depends=["TestWorldModeling::test_create_world_default"])
     def test_create_room():
         """ Tests the creation of a room """
 
         room_name = "room1"
-        # room_coords = [(-1, -1), (1.5, -1), (1.5, 1.5), (0.5, 1.5)]
         room_coords = [(-8, -2), (-8, 2), (-6, 2), (-6, -2)]
         room1 = Room(room_coords,
                     name=room_name,
                     color=[1, 0, 0])
-        TestWorldModelling.world.add_room(room1)
+        TestWorldModeling.world.add_room(room1)
 
-        assert len(TestWorldModelling.world.rooms) == 1
-        assert TestWorldModelling.world.get_room_by_name(room_name) == room1
+        assert len(TestWorldModeling.world.rooms) == 1
+        assert TestWorldModeling.world.get_room_by_name(room_name) == room1
         
         room_name = "room2"
         room_coords = [(8, -2), (8, 2), (6, 2), (6, -2)]
         room2 = Room(room_coords,
                     name=room_name,
                     color=[1, 0, 0])
-        TestWorldModelling.world.add_room(room2)
-        assert len(TestWorldModelling.world.rooms) == 2
-        assert TestWorldModelling.world.get_room_by_name(room_name) == room2
+        TestWorldModeling.world.add_room(room2)
+        assert len(TestWorldModeling.world.rooms) == 2
+        assert TestWorldModeling.world.get_room_by_name(room_name) == room2
 
     @staticmethod
     @pytest.mark.dependency(depends=
-        ["TestWorldModelling::test_create_world_default","TestWorldModelling::test_create_room"])
+        ["TestWorldModeling::test_create_world_default","TestWorldModeling::test_create_room"])
     def test_create_hallway():
         """ Tests the creation of a hallway between 2 rooms"""
 
-        TestWorldModelling.world.add_hallway("room1", "room2", offset=0.5,
+        TestWorldModeling.world.add_hallway("room1", "room2", offset=0.5,
             conn_method="auto", width = 0.5)
-        assert len(TestWorldModelling.world.hallways) == 1
+        assert len(TestWorldModeling.world.hallways) == 1
