4aae78a35c9f84a84a22b6c1b235b0c7f0772e51
AndyZe <andyz@utexas.edu>
2023-02-04 22:56:41 -0600

    Add catkin-pkg comment

--- setup/create_python_env.bash
+++ setup/create_python_env.bash
@@ -16,6 +16,7 @@ echo "Created Python virtual environment in $VIRTUALENV_FOLDER"
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 pushd $SCRIPT_DIR/../pyrobosim
 python -m pip install --upgrade pip
+# TODO: why does `catkin-pkg` help resolve some weird colcon build issues?
 pip install catkin-pkg empy lark pytest pytest-dependency pytest-html wheel
 pip install .
 popd
