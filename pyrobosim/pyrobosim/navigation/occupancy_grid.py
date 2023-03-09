7aea28652fafc1b274dfb677a67caa64fd5ff77f
ibrahiminfinite <ibrahimjkd@@gmail.com>
2023-03-09 19:39:32 +0530

    refactor

--- pyrobosim/pyrobosim/navigation/occupancy_grid.py
+++ pyrobosim/pyrobosim/navigation/occupancy_grid.py
@@ -150,6 +150,50 @@ class OccupancyGrid:
         """
         return (not self.is_in_bounds(pos)) or self.data[pos[0], pos[1]] == 1
 
+    def connectable(self, pointA, pointB):
+        """
+        Checks if 2 points can be connected in a  straight line.
+
+        :param pointA: The source point in the grid
+        :type pointA: (int, int)
+        :param pointB: The destination point in the grid
+        :type pointB: (int, int)
+        :return: (True, lastpoint) if pointA can be connected to pointB, else (False, lastpoint),
+                 lastpoint is the last point that can be reached from the source in a straight line
+                 towards the destination.
+                 If pointA and pointB are connectable lastpoint will be pointB.
+        """
+     
+        x0,y0 = pointA
+        x1,y1 = pointB
+        dx = x1 - x0
+        dy = y1 - y0
+        xsign = 1 if dx > 0 else -1
+        ysign = 1 if dy > 0 else -1
+        dx = abs(dx)
+        dy = abs(dy)
+        if dx > dy:
+            xx, xy, yx, yy = xsign, 0, 0, ysign
+        else:
+            dx, dy = dy, dx
+            xx, xy, yx, yy = 0, ysign, xsign, 0
+
+        D = 2*dy - dx
+        y = 0
+        collision = False
+        last_free_point = pointA
+        for x in range(dx + 1):
+            position = (x0 + x*xx + y*yx, y0 + x*xy + y*yy)
+            if self.is_obstacle(position):
+                lastpoint = last_free_point
+                break               
+            if D >= 0:
+                y += 1
+                D -= 2*dx
+            D += 2*dy
+            last_free_point = position   
+        return collision, lastpoint
+
 
 def occupancy_grid_from_world(
     world,
