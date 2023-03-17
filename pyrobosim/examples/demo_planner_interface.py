17c7277c90a73b4c517b74c3c20e3624ec28e9fa
ibrahiminfinite <ibrahimjkd@gmail.com>
2023-03-17 11:08:54 +0530

    Add demo for new planner interface

---
+++ pyrobosim/examples/demo_planner_interface.py
@@ -0,0 +1,37 @@
+import os
+from pyrobosim.gui.main import start_gui
+from pyrobosim.utils.pose import Pose
+from pyrobosim.core.yaml import WorldYamlLoader
+from pyrobosim.utils.general import get_data_folder
+from pyrobosim.navigation.path_planner import PathPlanner
+from pyrobosim.navigation.occupancy_grid import occupancy_grid_from_world
+
+
+# Load a test world.
+data_folder = get_data_folder()
+loader = WorldYamlLoader()
+world = loader.from_yaml(os.path.join(data_folder, "test_world.yaml"))
+
+
+def find_path_grid():
+    """Creates an A* planner and plans a path using grid."""
+    robot = world.robots[0]
+    planner_config = {
+        "grid": occupancy_grid_from_world(
+            world, resolution=0.05, inflation_radius=1.5 * robot.radius
+        ),
+        "diagonal_motion": True,
+        "heuristic": "euclidean",
+    }
+
+    planner = PathPlanner("astar", planner_config)
+    start = Pose(x=-0.5, y=-0.5)
+    goal = Pose(x=3.0, y=3.0)
+    robot.set_pose(start)
+    robot.set_path_planner(planner)
+    robot.current_path = robot.plan_path(start, goal)
+
+
+if __name__ == "__main__":
+    find_path_grid()
+    start_gui(world)
