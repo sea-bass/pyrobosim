5d5bb5145e363cf358965113fae3ade248a83a16
Sebastian Castro <sebas.a.castro@gmail.com>
2023-03-26 23:45:17 -0400

    Do not import PDDLStream planner if module not available

--- pyrobosim/pyrobosim/planning/__init__.py
+++ pyrobosim/pyrobosim/planning/__init__.py
@@ -6,4 +6,8 @@ a plan, as well as infrastructure to come up with such a plan given the
 current and desired state of the world.
 """
 
-from .pddlstream.planner import *
+import importlib
+
+# Only import PDDLStream planner if the module is available.
+if importlib.util.find_spec("pddlstream") is not None:
+    from .pddlstream.planner import *
