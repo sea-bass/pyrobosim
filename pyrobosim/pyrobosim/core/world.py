c553dc7d7b5fddc4061121bc5e5985709a709872
ibrahiminfinite <ibrahimjkd@gmail.com>
2023-04-21 14:13:09 +0530

    Revert random node selection

--- pyrobosim/pyrobosim/core/world.py
+++ pyrobosim/pyrobosim/core/world.py
@@ -3,7 +3,6 @@
 import itertools
 import numpy as np
 import warnings
-import random
 
 from .hallway import Hallway
 from .locations import Location, ObjectSpawn
@@ -1193,7 +1192,7 @@ class World:
         elif isinstance(entity, Object):
             graph_nodes = entity.parent.graph_nodes
         elif isinstance(entity, Location):
-            graph_nodes = random.choice(entity.children).graph_nodes
+            graph_nodes = entity.children[0].graph_nodes
         else:
             warnings.warn(f"Cannot get graph node from {entity}")
             return None
