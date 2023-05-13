292a538b6991e055a04ba32b3fc0532fa9722bcd
Sebastian Castro <sebas.a.castro@gmail.com>
2023-05-13 11:47:23 -0400

    Set QT_QPA_PLATFORM to offscreen for unit tests

--- test/run_tests.bash
+++ test/run_tests.bash
@@ -3,9 +3,11 @@
 # Runs all unit tests
 echo "Running unit tests..."
 
+export QT_QPA_PLATFORM=offscreen
+
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 TEST_RESULTS_DIR=$SCRIPT_DIR/results
 
-python3 -m pytest $SCRIPT_DIR \
- --junitxml=$TEST_RESULTS_DIR/test_results.xml \
- --html=$TEST_RESULTS_DIR/test_results.html --self-contained-html
+python3 -m pytest "$SCRIPT_DIR" \
+ --junitxml="$TEST_RESULTS_DIR/test_results.xml" \
+ --html="$TEST_RESULTS_DIR/test_results.html" --self-contained-html
