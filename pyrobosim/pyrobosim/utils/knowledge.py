e7899eb2b739d1df703cf54146478ccca4a4e311
Sebastian Castro <sebas.a.castro@gmail.com>
2023-05-25 00:36:53 -0400

    Remove extra debug print statement

--- pyrobosim/pyrobosim/utils/knowledge.py
+++ pyrobosim/pyrobosim/utils/knowledge.py
@@ -208,8 +208,6 @@ def resolve_to_location(
     :return: The location or object spawn that meets the category and/or room filters, or None.
     :rtype: :class:`pyrobosim.core.locations.Location`/:class:`pyrobosim.core.locations.ObjectSpawn`
     """
-    print(f"Resolving to location with category: {category}, room: {room}")
-
     if room is None:
         room_name = None
         if category is None:
