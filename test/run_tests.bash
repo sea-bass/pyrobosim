4a8eaf98b23147abb72e175ed31d4a0174ddfefc
Sebastian Castro <sebas.a.castro@gmail.com>
2023-07-12 10:21:43 -0400

    Fix test script

--- test/run_tests.bash
+++ test/run_tests.bash
@@ -9,23 +9,24 @@
 #   ./run_tests.bash ${ROS_DISTRO}
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-TEST_RESULTS_DIR=$SCRIPT_DIR/results
+TEST_RESULTS_DIR="${SCRIPT_DIR}/results"
 
 # Run regular pytest tests
 echo "Running Python package unit tests..."
-# python3 -m pytest "$SCRIPT_DIR" \
-#  --junitxml="$TEST_RESULTS_DIR/test_results.xml" \
-#  --html="$TEST_RESULTS_DIR/test_results.html" --self-contained-html
+python3 -m pytest "$SCRIPT_DIR" \
+ --junitxml="$TEST_RESULTS_DIR/test_results.xml" \
+ --html="$TEST_RESULTS_DIR/test_results.html" --self-contained-html
 
 echo ""
 
 # Run colcon tests, if using a ROS distro
 ROS_DISTRO=$1
-if [ -n "$ROS_DISTRO" ]
+if [[ -n "$ROS_DISTRO" && -n "$COLCON_PREFIX_PATH" ]]
 then
-    echo "Running ROS package unit tests..."
-    pushd "${SCRIPT_DIR}/../../.." || exit
+    WORKSPACE_DIR="${COLCON_PREFIX_PATH}/../"
+    echo "Running ROS package unit tests from ${WORKSPACE_DIR}..."
+    pushd "${WORKSPACE_DIR}" > /dev/null || exit
     colcon test --event-handlers console_cohesion+
     colcon test-result --verbose
-    popd || exit
+    popd > /dev/null || exit
 fi
