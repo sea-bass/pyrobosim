a5d9795d6121e7086c2cf333882d2acd2c697605
Henry Moore <henrygerardmoore@gmail.com>
2023-07-13 20:19:40 -0600

    added resolve to object test

--- test/utils/test_knowledge_utils.py
+++ test/utils/test_knowledge_utils.py
@@ -5,17 +5,29 @@ Test script for world knowledge utilities
 """
 
 import pytest
+import os
 import numpy as np
 
-import pyrobosim.core
+from pyrobosim.core import WorldYamlLoader
+from pyrobosim.core import Robot
+from pyrobosim.core import World
+
+# import functions to test
 from pyrobosim.utils.knowledge import (
     apply_resolution_strategy,
     query_to_entity,
     resolve_to_location,
     resolve_to_object,
 )
+
 from pyrobosim.utils.pose import Pose
-from pyrobosim.core import Robot
+from pyrobosim.utils.general import get_data_folder
+
+
+def load_world():
+    """Load a test world."""
+    world_file = os.path.join(get_data_folder(), "test_world.yaml")
+    return WorldYamlLoader().from_yaml(world_file)
 
 
 def test_apply_resolution_strategy():
@@ -107,7 +119,66 @@ def test_resolve_to_location():
 
 
 def test_resolve_to_object():
-    pass
+    test_world = load_world()
+
+    # test that we can get the first object added to the world
+    obj = resolve_to_object(test_world)
+    assert obj.name == "banana0"
+
+    for category in ["apple", "banana", "water", "coke"]:
+        # all of these can be found somewhere
+        obj = resolve_to_object(test_world, category=category)
+        assert obj.category == category
+
+    for location in ["table", "desk", "counter", "trash_can"]:
+        # all of these locations have objects
+        obj = resolve_to_object(test_world, location=location)
+        assert obj.parent.category == location
+
+    for room in ["kitchen", "bedroom", "bathroom"]:
+        # all of these rooms have objects
+        obj = resolve_to_object(test_world, room=room)
+        assert obj.parent.parent.parent.name == room
+
+    # now test specific objects
+
+    # set our position to be the same as a banana on the desk and make sure we find that
+    robot = Robot("test_robot")
+    robot.pose = Pose.from_list([3.2, 3.5, 0.0, 0.0])
+    obj = resolve_to_object(test_world, resolution_strategy="nearest", robot=robot)
+    assert obj.category == "apple" and obj.parent.parent.name == "my_desk"
+
+    # this shouldn't be the apple even though it's nearest because it doesn't fit the category
+    obj = resolve_to_object(
+        test_world, category="banana", resolution_strategy="nearest", robot=robot
+    )
+    assert not (obj.category == "apple" and obj.parent.parent.name == "my_desk")
+
+    # this shouldn't be the apple even though it's nearest because it doesn't fit the location
+    obj = resolve_to_object(
+        test_world, location="table", resolution_strategy="nearest", robot=robot
+    )
+    assert not (obj.category == "apple" and obj.parent.parent.name == "my_desk")
+
+    # this shouldn't be the apple even though it's nearest because it doesn't fit the room
+    obj = resolve_to_object(
+        test_world, room="bathroom", resolution_strategy="nearest", robot=robot
+    )
+    assert not (obj.category == "apple" and obj.parent.parent.name == "my_desk")
+
+    # if we pick up the nearest object we should find it when not ignoring grasped, but not when ignoring grasped
+    obj_nearest = resolve_to_object(
+        test_world, resolution_strategy="nearest", robot=robot
+    )
+    robot._attach_object(obj_nearest)
+    obj = resolve_to_object(
+        test_world, resolution_strategy="nearest", ignore_grasped=False, robot=robot
+    )
+    assert obj == obj_nearest
+    obj = resolve_to_object(
+        test_world, resolution_strategy="nearest", ignore_grasped=True, robot=robot
+    )
+    assert obj != obj_nearest
 
 
 if __name__ == "__main__":
