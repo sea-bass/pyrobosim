38188dfc9c8c39124fb6e0d15fe7948fd1e7b880
Henry Moore <henrygerardmoore@gmail.com>
2023-07-29 17:42:46 -0600

    applied PR feedback

--- test/utils/test_knowledge_utils.py
+++ test/utils/test_knowledge_utils.py
@@ -6,6 +6,7 @@ Test script for world knowledge utilities
 
 import os
 import numpy as np
+import pytest
 
 from pyrobosim.core import WorldYamlLoader
 from pyrobosim.core import Robot
@@ -23,6 +24,10 @@ from pyrobosim.utils.pose import Pose
 from pyrobosim.utils.general import get_data_folder
 
 
+class MockEntity:
+    pose = Pose()
+
+
 def load_world():
     """Load a test world."""
     world_file = os.path.join(get_data_folder(), "test_world.yaml")
@@ -32,80 +37,60 @@ def load_world():
 def test_apply_resolution_strategy():
     # First, test all strategies with empty entity_list
     entity_list = []
-    entity = apply_resolution_strategy([], entity_list, "first")
+    entity = apply_resolution_strategy(entity_list, "first")
     assert entity is None
-    entity = apply_resolution_strategy([], entity_list, "random")
+    entity = apply_resolution_strategy(entity_list, "random")
     assert entity is None
-    entity = apply_resolution_strategy([], entity_list, "nearest")
+    entity = apply_resolution_strategy(entity_list, "nearest")
     assert entity is None
 
     # Test non-existent strategy
     entity_list = ["don't return this"]
-    # catch the warning as an error
-    import warnings
 
-    warnings.filterwarnings("error")
-    try:
-        entity = apply_resolution_strategy([], entity_list, "non-existent")
-    except UserWarning:
-        # test passed, this should warn
-        pass
-    else:
-        assert (
-            False
-        ), "apply_resolution_strategy didn't warn on non-implemented resolution strategy"
+    with pytest.warns(UserWarning):
+        entity = apply_resolution_strategy(entity_list, "non-existent")
 
+
+def test_apply_first_resolution_strategy():
     # Test 'first' strategy
     entity_list = ["First", "Second"]
-    entity = apply_resolution_strategy([], entity_list, "first")
+    entity = apply_resolution_strategy(entity_list, "first")
     assert entity == "First"
-
-    entity_list = [["list"], "second"]
-    entity = apply_resolution_strategy([], entity_list, "first")
-    assert entity == ["list"]
 
     # Test 'random' strategy
     entity_list = ["First", "Second", "Third"]
     for _ in range(0, 3):
         # try multiple times
-        entity = apply_resolution_strategy([], entity_list, "first")
+        entity = apply_resolution_strategy(entity_list, "random")
         assert entity in entity_list
 
     entity_list = ["Only"]
-    entity = apply_resolution_strategy([], entity_list, "first")
+    entity = apply_resolution_strategy(entity_list, "first")
     assert entity == "Only"
 
+
+def test_apply_nearest_resolution_strategy():
     # Test 'nearest' strategy
     # Test that no robot warns
     entity_list = ["Only"]
-    try:
-        entity = apply_resolution_strategy([], entity_list, "nearest", None)
-    except UserWarning:
-        # test passed, this should warn
-        pass
-    else:
-        assert (
-            False
-        ), "apply_resolution_strategy didn't warn on non-existent robot strategy"
+    with pytest.warns(UserWarning):
+        entity = apply_resolution_strategy(entity_list, "nearest", None)
 
     robot = Robot("test_robot")
 
-    class Entity:
-        pose = Pose()
-
-    entity_list = [Entity(), Entity(), Entity()]
-    entity_list[0].pose = Pose.from_list([1, 0])
-    entity_list[1].pose = Pose.from_list([2, 0])
-    entity_list[2].pose = Pose.from_list([3, 0])
-    entity = apply_resolution_strategy([], entity_list, "nearest", robot)
+    entity_list = [MockEntity(), MockEntity(), MockEntity()]
+    entity_list[0].pose = Pose(x=1, y=0)
+    entity_list[1].pose = Pose(x=2, y=0)
+    entity_list[2].pose = Pose(x=3, y=0)
+    entity = apply_resolution_strategy(entity_list, "nearest", robot)
     assert entity == entity_list[0]
 
-    robot.pose = Pose.from_list([2, 1])
-    entity = apply_resolution_strategy([], entity_list, "nearest", robot)
+    robot.pose = Pose(x=2, y=1)
+    entity = apply_resolution_strategy(entity_list, "nearest", robot)
     assert entity == entity_list[1]
 
-    robot.pose = Pose.from_list([1000, -1000])
-    entity = apply_resolution_strategy([], entity_list, "nearest", robot)
+    robot.pose = Pose(x=1000, y=-1000)
+    entity = apply_resolution_strategy(entity_list, "nearest", robot)
     assert entity == entity_list[2]
 
 
@@ -127,7 +112,7 @@ def test_query_to_entity():
 
     # nearest object on the kitchen table should be the banana
     robot = Robot("test_robot")
-    robot.pose = Pose.from_list([1.0, -0.5])
+    robot.pose = Pose(x=1.0, y=-0.5)
     query = "kitchen table"
     entity = query_to_entity(test_world, query.split(), "object", "nearest", robot)
     assert (
@@ -138,38 +123,27 @@ def test_query_to_entity():
 
     # we want the nearest apple on the kitchen table, should no longer return the banana even though it is the nearest object
     query = "kitchen table apple"
-    new_entity = query_to_entity(test_world, query.split(), "object", "nearest", robot)
-    assert new_entity != entity
+    entity = query_to_entity(test_world, query.split(), "object", "nearest", robot)
+    assert entity.name == "gala"
 
     # things that should warn
-    import warnings
-
-    warnings.filterwarnings("error")
     query = "kitchen table fake"
     # search for nonexistent object in real location
     for mode in ["object", "location"]:
-        try:
+        with pytest.warns(UserWarning):
             entity = query_to_entity(test_world, query.split(), mode)
-        except UserWarning:
-            # test passed, this should warn
-            pass
-        else:
-            assert False, "resolve_to_location didn't warn when not finding an object"
 
     # search for absolute garbage
     query = "fake fake fake"
     for mode in ["object", "location"]:
-        try:
+        with pytest.warns(UserWarning):
             entity = query_to_entity(test_world, query.split(), mode)
-        except UserWarning:
-            # test passed, this should warn
-            pass
-        else:
-            assert False, "resolve_to_location didn't warn when not finding an object"
 
 
 def test_resolve_to_location():
     test_world = load_world()
+
+    # table0 is the first location in the test world
     loc = resolve_to_location(test_world)
     assert loc.name == "table0"
 
@@ -186,7 +160,7 @@ def test_resolve_to_location():
     # put ourselves on a table and make sure it is the nearest one
     # then, search for a different category and make sure we find something that isn't the same table we're on
     robot = Robot("test_robot")
-    robot.pose = Pose.from_list([0.85, -0.5, 0.0, -1.57])
+    robot.pose = Pose(x=0.85, y=-0.5)
     nearest_loc = resolve_to_location(
         test_world, resolution_strategy="nearest", robot=robot
     )
@@ -194,7 +168,7 @@ def test_resolve_to_location():
     loc = resolve_to_location(
         test_world, category="desk", resolution_strategy="nearest", robot=robot
     )
-    assert loc != nearest_loc
+    assert loc.name == "my_desk"
 
     # test expanding locations
     loc = resolve_to_location(
@@ -207,36 +181,13 @@ def test_resolve_to_location():
     assert loc.parent.category == "desk"
 
     # things that should warn
-    import warnings
-
-    warnings.filterwarnings("error")
-
     # fake location category
-    try:
+    with pytest.warns(UserWarning):
         loc = resolve_to_location(test_world, category="fake")
-    except UserWarning:
-        # test passed, this should warn
-        pass
-    else:
-        assert False, "resolve_to_location didn't warn when not finding an object"
-
-    # fake location category
-    try:
-        loc = resolve_to_location(test_world, room="fake")
-    except UserWarning:
-        # test passed, this should warn
-        pass
-    else:
-        assert False, "resolve_to_location didn't warn when not finding an object"
 
     # combination that doesn't exist
-    try:
+    with pytest.warns(UserWarning):
         loc = resolve_to_location(test_world, room="bedroom", category="trash_can")
-    except UserWarning:
-        # test passed, this should warn
-        pass
-    else:
-        assert False, "resolve_to_location didn't warn when not finding an object"
 
 
 def test_resolve_to_object():
@@ -265,7 +216,7 @@ def test_resolve_to_object():
 
     # set our position to be the same as a banana on the desk and make sure we find that
     robot = Robot("test_robot")
-    robot.pose = Pose.from_list([3.2, 3.5, 0.0, 0.0])
+    robot.pose = Pose(x=3.2, y=3.5)
     obj = resolve_to_object(test_world, resolution_strategy="nearest", robot=robot)
     assert obj.category == "apple" and obj.parent.parent.name == "my_desk"
 
@@ -288,39 +239,20 @@ def test_resolve_to_object():
     assert not (obj.category == "apple" and obj.parent.parent.name == "my_desk")
 
     # things that should warn
-    import warnings
-
-    warnings.filterwarnings("error")
-
     # fake object category
-    try:
+    with pytest.warns(UserWarning):
         obj = resolve_to_object(test_world, category="fake")
-    except UserWarning:
-        # test passed, this should warn
-        pass
-    else:
-        assert False, "resolve_to_object didn't warn when not finding an object"
 
     # fake location
-    try:
+    with pytest.warns(UserWarning):
         obj = resolve_to_object(test_world, location="fake")
-    except UserWarning:
-        # test passed, this should warn
-        pass
-    else:
-        assert False, "resolve_to_object didn't warn when not finding an object"
 
     # fake room
-    try:
+    with pytest.warns(UserWarning):
         obj = resolve_to_object(test_world, robot=robot, room="fake")
-    except UserWarning:
-        # test passed, this should warn
-        pass
-    else:
-        assert False, "resolve_to_object didn't warn when not finding an object"
 
     # combination that doesn't exist
-    try:
+    with pytest.warns(UserWarning):
         obj = resolve_to_object(
             test_world,
             robot=robot,
@@ -328,11 +260,6 @@ def test_resolve_to_object():
             category="banana",
             location="trash_can",
         )
-    except UserWarning:
-        # test passed, this should warn
-        pass
-    else:
-        assert False, "resolve_to_object didn't warn when not finding an object"
 
     # if we pick up the nearest object we should find it when not ignoring grasped, but not when ignoring grasped
     # test this last because it changes the state of the world
@@ -351,7 +278,9 @@ def test_resolve_to_object():
 
 
 if __name__ == "__main__":
-    test_apply_resolution_strategy()
+    test_apply_nearest_resolution_strategy()
+    test_apply_first_resolution_strategy()
+    test_apply_nearest_resolution_strategy()
     test_query_to_entity()
     test_resolve_to_location()
     test_resolve_to_object()
