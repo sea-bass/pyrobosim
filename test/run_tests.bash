ea5a65e107938ff091dc32376aaaf9e55d3e1900
Sebastian Castro <sebas.a.castro@gmail.com>
2023-08-06 11:26:50 -0400

    Add coverage collection to tests

--- test/run_tests.bash
+++ test/run_tests.bash
@@ -14,8 +14,12 @@ TEST_RESULTS_DIR="${SCRIPT_DIR}/results"
 # Run regular pytest tests
 echo "Running Python package unit tests..."
 python3 -m pytest "$SCRIPT_DIR" \
+ --cov="$SCRIPT_DIR" --cov-branch --cov-report term \
+ --cov-report html:"$TEST_RESULTS_DIR/test_results_coverage_html" \
+ --cov-report xml:"$TEST_RESULTS_DIR/test_results_coverage.xml" \
  --junitxml="$TEST_RESULTS_DIR/test_results.xml" \
- --html="$TEST_RESULTS_DIR/test_results.html" --self-contained-html
+ --html="$TEST_RESULTS_DIR/test_results.html" \
+ --self-contained-html
 
 echo ""
 
@@ -26,7 +30,9 @@ then
     WORKSPACE_DIR="${COLCON_PREFIX_PATH}/../"
     echo "Running ROS package unit tests from ${WORKSPACE_DIR}..."
     pushd "${WORKSPACE_DIR}" > /dev/null || exit
-    colcon test --event-handlers console_cohesion+
+    colcon test \
+        --event-handlers console_cohesion+ \
+        --pytest-with-coverage --pytest-args " --cov-report term"
     colcon test-result --verbose
     popd > /dev/null || exit
 fi
