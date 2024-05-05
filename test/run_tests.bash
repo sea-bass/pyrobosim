44e52be8fc56dc4b5ab14c99739869a8dc90e54d
Sebastian Castro <sebas.a.castro@gmail.com>
2024-05-05 18:50:51 -0400

    Colcon test and virtual environments are a match made in the lowest depths of hell

--- test/run_tests.bash
+++ test/run_tests.bash
@@ -10,7 +10,7 @@
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 TEST_RESULTS_DIR="${SCRIPT_DIR}/results"
-mkdir -p "$TEST_RESULTS_DIR"
+mkdir -p "${TEST_RESULTS_DIR}"
 
 # Ensure we run everything for coverage purposes, but ensure failures are returned by the script
 SUCCESS=0
@@ -20,30 +20,29 @@ set -o pipefail
 
 # Run regular pytest tests
 echo "Running Python package unit tests..."
-python3 -m pytest "$SCRIPT_DIR" \
- --cov="$SCRIPT_DIR/../pyrobosim/pyrobosim" --cov-branch \
+python3 -m pytest "${SCRIPT_DIR}" \
+ --cov="${SCRIPT_DIR}/../pyrobosim/pyrobosim" --cov-branch \
  --cov-report term \
- --cov-report html:"$TEST_RESULTS_DIR/test_results_coverage_html" \
- --cov-report xml:"$TEST_RESULTS_DIR/test_results_coverage.xml" \
- --junitxml="$TEST_RESULTS_DIR/test_results.xml" \
- --html="$TEST_RESULTS_DIR/test_results.html" \
+ --cov-report html:"${TEST_RESULTS_DIR}/test_results_coverage_html" \
+ --cov-report xml:"${TEST_RESULTS_DIR}/test_results_coverage.xml" \
+ --junitxml="${TEST_RESULTS_DIR}/test_results.xml" \
+ --html="${TEST_RESULTS_DIR}/test_results.html" \
  --self-contained-html \
- | tee "$TEST_RESULTS_DIR"/pytest-coverage.txt || SUCCESS=$?
+ | tee "${TEST_RESULTS_DIR}/pytest-coverage.txt" || SUCCESS=$?
 echo ""
 
-# Run colcon tests, if using a ROS distro
+# Run ROS package tests, if using a ROS distro
 ROS_DISTRO=$1
-if [[ -n "$ROS_DISTRO" && -n "$COLCON_PREFIX_PATH" ]]
+if [[ -n "${ROS_DISTRO}" && -n "${COLCON_PREFIX_PATH}" ]]
 then
     WORKSPACE_DIR="${COLCON_PREFIX_PATH}/../"
-    echo "Running ROS package unit tests from ${WORKSPACE_DIR}..."
-    pushd "${WORKSPACE_DIR}" > /dev/null || exit
-    colcon test \
-        --event-handlers console_cohesion+ \
-        --pytest-with-coverage --pytest-args " --cov-report term" || SUCCESS=$?
-echo ""
-    colcon test-result --verbose
-    popd > /dev/null || exit
+    ROS_PKG_DIR="${WORKSPACE_DIR}/src/pyrobosim_ros"
+    echo "Running ROS package unit tests from ${ROS_PKG_DIR} ..."
+    python3 -m pytest "${ROS_PKG_DIR}" \
+     --junitxml="${TEST_RESULTS_DIR}/test_results_ros.xml" \
+     --html="${TEST_RESULTS_DIR}/test_results_ros.html" \
+     --self-contained-html || SUCCESS=$?
+    echo ""
 fi
 
-exit $SUCCESS
+exit ${SUCCESS}
