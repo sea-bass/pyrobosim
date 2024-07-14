ce15c3db8911bf4ec1cdb9cd2bdcaa0a26513b3e
Sebastian Castro <sebas.a.castro@gmail.com>
2024-07-14 14:17:18 -0400

    Fix test typo

--- test/system/test_system.py
+++ test/system/test_system.py
@@ -60,7 +60,7 @@ class TestSystem:
             time.sleep(0.2)
         robot.location = world.get_location_from_pose(robot.get_pose())
 
-        assert robot.last_nav_status.is_success()
+        assert robot.last_nav_result.is_success()
         assert (
             robot.location == expected_location
             or robot.location in expected_location.children
