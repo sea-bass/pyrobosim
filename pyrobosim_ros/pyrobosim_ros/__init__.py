a68ba2bbf17d2a5e144f9a3352aac4d66876f5a2
Sebastian Castro <sebas.a.castro@gmail.com>
2024-07-24 21:50:21 -0400

    Delete pyrobosim_ros and pyrobosim_msgs packages

--- pyrobosim_ros/pyrobosim_ros/__init__.py
+++
@@ -1,5 +0,0 @@
-""" ROS 2 interface to pyrobosim.
-
-This module contains tools for using pyrobosim with ROS 2, including interfaces
-to world and robot models, as well as general conversion utilities.
-"""
