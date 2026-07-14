bc303f68ca4cd643e6aebd06226183c509b9431d
Sebastian Castro <sebas.a.castro@gmail.com>
2026-07-14 07:50:43 -0400

    4.4.0

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = "2022-2026, Sebastian Castro"
 author = "Sebastian Castro"
 
 # The full version, including alpha/beta/rc tags
-version = release = "4.3.4"
+version = release = "4.4.0"
 
 
 # -- General configuration ---------------------------------------------------
